40a87cdd3381f9cdb4a0316fc4ef8c5955b966f7
Alex999999sndjsan <a22alejandro.villedaa7@cbtis187.edu.mx>
2024-05-08 11:57:40 -0600

    Add files via upload

---
+++ letras.js
@@ -0,0 +1,500 @@
+
+function cambiarColor() {
+   var boton = document.getElementById('miBotonA');
+   if (boton.classList.contains('botonAzul-1')) {
+     boton.classList.remove('botonAzul-1');
+     boton.classList.add('botonRojo-1');
+   } else {
+     boton.classList.remove('botonRojo-1');
+     boton.classList.add('botonAzul-1');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'a' || event.key === 'A') {
+     cambiarColor();
+   }
+ });
+ function cambiarColor2() {
+   var boton = document.getElementById('miBotonB');
+   if (boton.classList.contains('botonAzul-2')) {
+     boton.classList.remove('botonAzul-2');
+     boton.classList.add('botonRojo-2');
+   } else {
+     boton.classList.remove('botonRojo-2');
+     boton.classList.add('botonAzul-2');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'b' || event.key === 'B') {
+     cambiarColor2();
+   }
+ });
+ function cambiarColor3() {
+   var boton = document.getElementById('miBotonC');
+   if (boton.classList.contains('botonAzul-3')) {
+     boton.classList.remove('botonAzul-3');
+     boton.classList.add('botonRojo-3');
+   } else {
+     boton.classList.remove('botonRojo-3');
+     boton.classList.add('botonAzul-3');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'c' || event.key === 'C') {
+     cambiarColor3();
+   }
+ });
+ function cambiarColor4() {
+   var boton = document.getElementById('miBotonD');
+   if (boton.classList.contains('botonAzul-4')) {
+     boton.classList.remove('botonAzul-4');
+     boton.classList.add('botonRojo-4');
+   } else {
+     boton.classList.remove('botonRojo-4');
+     boton.classList.add('botonAzul-4');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'd' || event.key === 'D') {
+     cambiarColor4();
+   }
+ });
+ function cambiarColor5() {
+   var boton = document.getElementById('miBotonE');
+   if (boton.classList.contains('botonAzul-5')) {
+     boton.classList.remove('botonAzul-5');
+     boton.classList.add('botonRojo-5');
+   } else {
+     boton.classList.remove('botonRojo-5');
+     boton.classList.add('botonAzul-5');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'e' || event.key === 'E') {
+     cambiarColor5();
+   }
+ });
+ function cambiarColor6() {
+   var boton = document.getElementById('miBotonF');
+   if (boton.classList.contains('botonAzul-6')) {
+     boton.classList.remove('botonAzul-6');
+     boton.classList.add('botonRojo-6');
+   } else {
+     boton.classList.remove('botonRojo-6');
+     boton.classList.add('botonAzul-6');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'f' || event.key === 'F') {
+     cambiarColor6();
+   }
+ });
+ function cambiarColor7() {
+   var boton = document.getElementById('miBotonG');
+   if (boton.classList.contains('botonAzul-7')) {
+     boton.classList.remove('botonAzul-7');
+     boton.classList.add('botonRojo-7');
+   } else {
+     boton.classList.remove('botonRojo-7');
+     boton.classList.add('botonAzul-7');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'g' || event.key === 'G') {
+     cambiarColor7();
+   }
+ });
+ function cambiarColor8() {
+   var boton = document.getElementById('miBotonH');
+   if (boton.classList.contains('botonAzul-8')) {
+     boton.classList.remove('botonAzul-8');
+     boton.classList.add('botonRojo-8');
+   } else {
+     boton.classList.remove('botonRojo-8');
+     boton.classList.add('botonAzul-8');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'h' || event.key === 'H') {
+     cambiarColor8();
+   }
+ });
+ function cambiarColor9() {
+   var boton = document.getElementById('miBotonI');
+   if (boton.classList.contains('botonAzul-9')) {
+     boton.classList.remove('botonAzul-9');
+     boton.classList.add('botonRojo-9');
+   } else {
+     boton.classList.remove('botonRojo-9');
+     boton.classList.add('botonAzul-9');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'i' || event.key === 'I') {
+     cambiarColor9();
+   }
+ });
+ function cambiarColor10() {
+   var boton = document.getElementById('miBotonJ');
+   if (boton.classList.contains('botonAzul-10')) {
+     boton.classList.remove('botonAzul-10');
+     boton.classList.add('botonRojo-10');
+   } else {
+     boton.classList.remove('botonRojo-10');
+     boton.classList.add('botonAzul-10');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'j' || event.key === 'J') {
+     cambiarColor10();
+   }
+ });
+ function cambiarColor11() {
+   var boton = document.getElementById('miBotonK');
+   if (boton.classList.contains('botonAzul-11')) {
+     boton.classList.remove('botonAzul-11');
+     boton.classList.add('botonRojo-11');
+   } else {
+     boton.classList.remove('botonRojo-11');
+     boton.classList.add('botonAzul-11');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'k' || event.key === 'K') {
+     cambiarColor11();
+   }
+ });
+ function cambiarColor12() {
+   var boton = document.getElementById('miBotonL');
+   if (boton.classList.contains('botonAzul-12')) {
+     boton.classList.remove('botonAzul-12');
+     boton.classList.add('botonRojo-12');
+   } else {
+     boton.classList.remove('botonRojo-12');
+     boton.classList.add('botonAzul-12');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'l' || event.key === 'L') {
+     cambiarColor12();
+   }
+ });
+ function cambiarColor13() {
+   var boton = document.getElementById('miBotonM');
+   if (boton.classList.contains('botonAzul-13')) {
+     boton.classList.remove('botonAzul-13');
+     boton.classList.add('botonRojo-13');
+   } else {
+     boton.classList.remove('botonRojo-13');
+     boton.classList.add('botonAzul-13');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'm' || event.key === 'M') {
+     cambiarColor13();
+   }
+ });
+ function cambiarColor14() {
+   var boton = document.getElementById('miBotonN');
+   if (boton.classList.contains('botonAzul-14')) {
+     boton.classList.remove('botonAzul-14');
+     boton.classList.add('botonRojo-14');
+   } else {
+     boton.classList.remove('botonRojo-14');
+     boton.classList.add('botonAzul-14');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'n' || event.key === 'N') {
+     cambiarColor14();
+   }
+ });
+ function cambiarColor15() {
+   var boton = document.getElementById('miBotonÑ');
+   if (boton.classList.contains('botonAzul-15')) {
+     boton.classList.remove('botonAzul-15');
+     boton.classList.add('botonRojo-15');
+   } else {
+     boton.classList.remove('botonRojo-15');
+     boton.classList.add('botonAzul-15');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'ñ' || event.key === 'Ñ') {
+     cambiarColor15();
+   }
+ });
+ function cambiarColor16() {
+   var boton = document.getElementById('miBotonO');
+   if (boton.classList.contains('botonAzul-16')) {
+     boton.classList.remove('botonAzul-16');
+     boton.classList.add('botonRojo-16');
+   } else {
+     boton.classList.remove('botonRojo-16');
+     boton.classList.add('botonAzul-16');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'o' || event.key === 'O') {
+     cambiarColor16();
+   }
+ });
+ function cambiarColor17() {
+   var boton = document.getElementById('miBotonP');
+   if (boton.classList.contains('botonAzul-17')) {
+     boton.classList.remove('botonAzul-17');
+     boton.classList.add('botonRojo-17');
+   } else {
+     boton.classList.remove('botonRojo-17');
+     boton.classList.add('botonAzul-17');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'p' || event.key === 'P') {
+     cambiarColor17();
+   }
+ });
+ function cambiarColor18() {
+   var boton = document.getElementById('miBotonQ');
+   if (boton.classList.contains('botonAzul-18')) {
+     boton.classList.remove('botonAzul-18');
+     boton.classList.add('botonRojo-18');
+   } else {
+     boton.classList.remove('botonRojo-18');
+     boton.classList.add('botonAzul-18');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'q' || event.key === 'Q') {
+     cambiarColor18();
+   }
+ });
+ function cambiarColor19() {
+   var boton = document.getElementById('miBotonS');
+   if (boton.classList.contains('botonAzul-19')) {
+     boton.classList.remove('botonAzul-19');
+     boton.classList.add('botonRojo-19');
+   } else {
+     boton.classList.remove('botonRojo-19');
+     boton.classList.add('botonAzul-19');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 's' || event.key === 'S') {
+     cambiarColor19();
+   }
+ });
+ function cambiarColor20() {
+   var boton = document.getElementById('miBotonT');
+   if (boton.classList.contains('botonAzul-20')) {
+     boton.classList.remove('botonAzul-20');
+     boton.classList.add('botonRojo-20');
+   } else {
+     boton.classList.remove('botonRojo-20');
+     boton.classList.add('botonAzul-20');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 't' || event.key === 'T') {
+     cambiarColor20();
+   }
+ });
+ function cambiarColor21() {
+   var boton = document.getElementById('miBotonU');
+   if (boton.classList.contains('botonAzul-21')) {
+     boton.classList.remove('botonAzul-21');
+     boton.classList.add('botonRojo-21');
+   } else {
+     boton.classList.remove('botonRojo-21');
+     boton.classList.add('botonAzul-21');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'u' || event.key === 'U') {
+     cambiarColor21();
+   }
+ });
+ function cambiarColor22() {
+   var boton = document.getElementById('miBotonV');
+   if (boton.classList.contains('botonAzul-22')) {
+     boton.classList.remove('botonAzul-22');
+     boton.classList.add('botonRojo-22');
+   } else {
+     boton.classList.remove('botonRojo-22');
+     boton.classList.add('botonAzul-22');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'v' || event.key === 'V') {
+     cambiarColor22();
+   }
+ });
+ function cambiarColor23() {
+   var boton = document.getElementById('miBotonW');
+   if (boton.classList.contains('botonAzul-23')) {
+     boton.classList.remove('botonAzul-23');
+     boton.classList.add('botonRojo-23');
+   } else {
+     boton.classList.remove('botonRojo-23');
+     boton.classList.add('botonAzul-23');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'w' || event.key === 'W') {
+     cambiarColor23();
+   }
+ });
+ function cambiarColor24() {
+   var boton = document.getElementById('miBotonX');
+   if (boton.classList.contains('botonAzul-24')) {
+     boton.classList.remove('botonAzul-24');
+     boton.classList.add('botonRojo-24');
+   } else {
+     boton.classList.remove('botonRojo-24');
+     boton.classList.add('botonAzul-24');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'x' || event.key === 'X') {
+     cambiarColor24();
+   }
+ });
+ function cambiarColor25() {
+   var boton = document.getElementById('miBotonY');
+   if (boton.classList.contains('botonAzul-25')) {
+     boton.classList.remove('botonAzul-25');
+     boton.classList.add('botonRojo-25');
+   } else {
+     boton.classList.remove('botonRojo-25');
+     boton.classList.add('botonAzul-25');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'y' || event.key === 'Y') {
+     cambiarColor25();
+   }
+ });
+ function cambiarColor26() {
+   var boton = document.getElementById('miBotonZ');
+   if (boton.classList.contains('botonAzul-26')) {
+     boton.classList.remove('botonAzul-26');
+     boton.classList.add('botonRojo-26');
+   } else {
+     boton.classList.remove('botonRojo-26');
+     boton.classList.add('botonAzul-26');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'z' || event.key === 'Z') {
+     cambiarColor26();
+   }
+ });
+ function cambiarColor18_5() {
+   var boton = document.getElementById('miBotonR');
+   if (boton.classList.contains('botonAzul-18_5')) {
+     boton.classList.remove('botonAzul-18_5');
+     boton.classList.add('botonRojo-18_5');
+   } else {
+     boton.classList.remove('botonRojo-18_5');
+     boton.classList.add('botonAzul-18_5');
+   }
+ }
+ document.addEventListener('keydown', function(event) {
+   if (event.key === 'r' || event.key === 'R') {
+     cambiarColor18_5();
+   }
+ });
+
+var words = ['programar','principio','cualquier','industria','encuentro','increible','etiquetas','minecraft','tungsteno','estrellas','carnicera','encantada','terragrim','semillera','clorofita','excalibur','psicopata'];
+var word = '';
+var guess = [];
+var attempts = [];
+var h2 = document.querySelector('h2');
+var output = document.getElementById('output');
+var input = document.getElementById('input');
+var man = [
+  ' ----|  <br> |      <br> |      <br> |      <br> |      <br> |      <br>--------',
+  ' ----|  <br> |  😄  <br> |      <br> |      <br> |      <br> |      <br>--------',
+  ' ----|  <br> |  😥  <br> |   |  <br> |      <br> |      <br> |      <br>--------',
+  ' ----|  <br> |  😰  <br> |   |  <br> |   |  <br> |      <br> |      <br>--------',
+  ' ----|  <br> |  😩  <br> |  \\|  <br> |   |  <br> |      <br> |      <br>--------',
+  ' ----|  <br> |  😫  <br> |  \\|/ <br> |   |  <br> |      <br> |      <br>--------',
+  ' ----|  <br> |  😱  <br> |  \\|/  <br> |   |  <br> |  /   <br> |      <br>--------',
+  ' ----|  <br> |  💀  <br> |  \\|/  <br> |   |  <br> |  / \\ <br> |      <br>--------'
+]
+
+function getRandomWord(){
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function NuevoJuego(){
+    word = getRandomWord();
+    guess= [];
+    attempts= [];
+    output.innerHTML = '';
+    resetearColores();
+    ActualizarP();
+}
+
+function resetearColores() {
+  for (let i = 1; i <= 26; i++) {
+    let boton = document.getElementById('miBoton' + String.fromCharCode(64 + i));
+    boton.classList.remove('botonRojo-' + i);
+    boton.classList.add('botonAzul-' + i);
+  }
+}
+
+function ActualizarP(){
+    var current = '';
+    input.value = '';
+    for(i=0; i<word.length; i++){
+        if (word[i] == ' '){
+            current += '<br>';
+        } else if(guess.indexOf(word[i]) !== -1){
+            current += '<span>' + word[i]+'</span>';
+        }
+        else
+        {
+            current += '<span> </span>';
+        }
+    }
+    h2.innerHTML = current;
+    output.innerHTML = man[attempts.length];
+    if(current.indexOf('> <') == -1){
+        alert("¡¡Ganaste!!");
+    }
+    if(attempts.length>=7){
+        alert("Perdiste la palabra era: "+word);
+    }
+
+    input.focus()
+}
+
+function AdivinarL(){
+    if(input.value !== undefined){
+        if(attempts.length < 7){
+            if(word.indexOf(input.value)!== -1){
+                alert("Correcto");
+                guess.push(input.value.toLowerCase());
+            }else{
+                attempts.push(input.value.toLowerCase());
+                alert("Incorrecto,"+"Te Quedan: "+attempts.length+"/7");
+            }
+            ActualizarP();
+        }
+    }
+}
+document.onclick = function(){
+    input.focus();
+}
+document.onkeypress = function(e){
+    if(e.keyCode == '27'){
+        var closeAll = document.querySelectorAll('[data-modal');
+        for(i=0; i<closeAll.length; i++){
+            var id = closeAll[i].getAttribute('data-modal');
+            destroyModal(id)
+        }
+    }
+}
+NuevoJuego();
+
